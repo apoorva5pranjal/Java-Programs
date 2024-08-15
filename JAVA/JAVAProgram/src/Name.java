52c9e2b414ce7b0abb7b30af3ad308d481737634
apoorva5pranjal <129883186+apoorva5pranjal@users.noreply.github.com>
2024-08-15 15:25:01 +0530

    Add files via upload

---
+++ JAVA/JAVAProgram/src/Name.java
@@ -0,0 +1,14 @@
+public class Name {
+    // Method to print the name
+    public void printMyName() {
+        System.out.println("Your Name Here"); // Replace "Your Name Here" with your actual name
+    }
+
+    public static void main(String[] args) {
+        // Create an instance of the Name class
+        Name myName = new Name();
+        // Call the printMyName method
+        myName.printMyName();
+    }
+}
+
---
+++ JAVA/JAVAProgram/src/naturalno.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class naturalno {
+    public static void main(String args[]){
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            int sum = 0;
+            for(int i = 0; i <= n; i++){
+                sum = sum+i;
+            }
+            System.out.println(sum);
+        }
+    }
+}
